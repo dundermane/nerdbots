2fee042c50d974671c6c3e21f0b36df8171d0ba0
matt <mewing6732@gmail.com>
2017-06-29 20:56:39 -0400

    added stupid function :

--- web/app.py
+++ web/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
+from planner import *
+
 
 app = Flask(__name__)
 
@@ -8,7 +10,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/ldata', methods=['GET'])
+def ldata():
+    return jsonify(get_last_movement_data(23))
