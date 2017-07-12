9aa3c39f6508edb0332c9a1525a7d1f9e5d1bd72
matt <mewing6732@gmail.com>
2017-07-11 22:40:02 -0400

    fixed ethans crappy code

--- hard/i2ctest.py
+++ hard/i2ctest.py
@@ -1,37 +1,39 @@
 import smbus
 import time
 bus = smbus.SMBus(1)
-address = hex(0x40)
+address = 0x40
 
-prescale = hex( round( ( 25000000 / ( 4096 * 50 ) ) - 1 )
-bus.write_byte_data(address, hex(254), prescale)
+prescale = ( 25000000 / ( 4096 * 50 ) ) - 1 
+print prescale
+bus.write_byte_data(address, 254, prescale)
 
 
 def setpwm( pos , value ):
     if pos in range(0,15):
-        onL = hex(0x00)
-        onH = hex(0x00)
-        offL = hex(value & 0x00FF)
-        offH = hex(value >> 2)
-        bus.write_byte_data(address, hex(6 + 4 * pos), onL)
-        bus.write_byte_data(address, hex(7 + 4 * pos), onH) 
-        bus.write_byte_data(address, hex(8 + 4 * pos), offL) 
-        bus.write_byte_data(address, hex(9 + 4 * pos), offH) 
+        onL = 0x00
+        onH = 0x00
+        offL = value & 0x00FF
+        offH = value >> 2
+        bus.write_byte_data(address, 6 + (4 * pos), onL)
+        bus.write_byte_data(address, 7 + (4 * pos), onH) 
+        bus.write_byte_data(address, 8 + (4 * pos), offL) 
+        bus.write_byte_data(address, 9 + (4 * pos), offH) 
  
 counter = 0
 ascending = 1
 
 while True:
-    setpwm(0, counter)
+    setpwm(1, counter)
+    print "set pwm at {0}".format(counter)
     if ascending and counter >= 4095:
         ascending = 0
         counter -= 1
-    else if ascending:
+    elif ascending:
         counter +=1
-    else if !ascending and <= 0:
+    elif not ascending and counter <= 0:
         ascending = 1
         counter += 1
-    else if !ascending:
+    elif not ascending:
         counter -= 1
     
     time.sleep(.01)
