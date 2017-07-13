53bd70e74601120c5b1c53f5d578448d99680e30
ebarrieau <ebarrieau@wpi.edu>
2017-07-13 02:33:09 +0000

    Updated i2c code to add degree to div function.  Need to fix floating point numbers.

--- hard/i2ctest.py
+++ hard/i2ctest.py
@@ -1,15 +1,18 @@
 import smbus
 import time
+
 bus = smbus.SMBus(1)
 address = 0x40
+frequency = 50
+
 
 def init():
     sleepbyte = 0b00010000
     bus.write_byte_data(address, 0, sleepbyte)
     time.sleep(.5)
 
-    prescale = ( 25000000 / ( 4096 * 50 ) ) - 1
-    bus.write_byte_data(address, 0xFE, prescale)
+    prescale = ( 25000000 / ( 4096 * frequency ) ) - 1
+    bus.write_byte_data(address, 0xFE , prescale)
 
     modebyte = bus.read_byte_data(address, 0)
     if (modebyte & 0b10000000) == 0b10000000:
@@ -23,6 +26,14 @@ def init():
 	print "Startup Failed"
         init()
 
+def todiv( degree, lowdegree, highdegree, lowtime, hightime ):
+    if (degree >= lowdegree and degree <= highdegree):
+        refresh = 1 / frequency
+        timediv = refresh / 4096
+        angletime = (hightime - lowtime) / (highdegree - lowdegree) * (degree - lowdegree) + lowtime
+        bitvalue = round(angletime / timediv)
+        return bitvalue
+    
 
 def setpwm( pos , value ):
     if pos in range(0,15):
@@ -38,22 +49,24 @@ def setpwm( pos , value ):
 	msb = bus.read_byte_data(address, 9)
 	print (msb << 8) + lsb        
 
-counter = 0
+counter = 200
 ascending = 1
 init()
 
 while True:
     setpwm(0, counter)
+    setpwm(1, counter)
+    setpwm(2, counter)
 
     if ascending and counter >= 500:
         ascending = 0
-        counter -= 1
+        time.sleep(0.5)
     elif ascending:
-        counter +=1
-    elif not ascending and counter <= 0:
+        counter +=5
+    elif not ascending and counter <=200:
         ascending = 1
-        counter += 1
+        time.sleep(0.5)
     elif not ascending:
-        counter -= 1
+        counter -= 5
     
-    time.sleep(0.005)
+    
